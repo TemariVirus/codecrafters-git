0708fdfbc848b4138e770eadab3fc5595690f3b6
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2025-04-26 23:00:23 +0800

    Read a blob object #ic4

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
-const stdout = std.io.getStdOut().writer();
+const fs = std.fs;
+
+var gpa: std.heap.DebugAllocator(.{}) = .init;
 
 pub fn main() !void {
-    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
+
+    const stdout_raw = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_raw);
+    const stdout = bw.writer().any();
 
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
@@ -15,17 +20,53 @@ pub fn main() !void {
     }
 
     const command: []const u8 = args[1];
-
-    if (std.mem.eql(u8, command, "init")) {
-        const cwd = std.fs.cwd();
-        _ = try cwd.makeDir("./.git");
-        _ = try cwd.makeDir("./.git/objects");
-        _ = try cwd.makeDir("./.git/refs");
-        {
-            const head = try cwd.createFile("./.git/HEAD", .{});
-            defer head.close();
-            _ = try head.write("ref: refs/heads/main\n");
+    if (strEql(command, "init")) {
+        try init(stdout);
+    } else if (strEql(command, "cat-file")) {
+        if (args.len < 4 or !strEql(args[2], "-p")) {
+            try std.io.getStdErr().writer().print("Usage: {s} cat-file -p <hash>\n", .{args[0]});
+            return;
         }
-        _ = try stdout.print("Initialized git directory\n", .{});
+        try catFile(stdout, args[3]);
+    }
+    try bw.flush();
+}
+
+fn strEql(a: []const u8, b: []const u8) bool {
+    return std.mem.eql(u8, a, b);
+}
+
+fn init(stdout: std.io.AnyWriter) !void {
+    const cwd = fs.cwd();
+    _ = try cwd.makeDir("./.git");
+    _ = try cwd.makeDir("./.git/objects");
+    _ = try cwd.makeDir("./.git/refs");
+    {
+        const head = try cwd.createFile("./.git/HEAD", .{});
+        defer head.close();
+        _ = try head.write("ref: refs/heads/main\n");
+    }
+    _ = try stdout.writeAll("Initialized git directory\n");
+}
+
+fn catFile(stdout: std.io.AnyWriter, hash: []const u8) !void {
+    const allocator = gpa.allocator();
+
+    const file = blk: {
+        const path = try std.fmt.allocPrint(allocator, ".git/objects/{s}/{s}", .{ hash[0..2], hash[2..] });
+        defer allocator.free(path);
+        break :blk try fs.cwd().openFile(path, .{});
+    };
+    defer file.close();
+
+    var br: std.io.BufferedReader(8192, @TypeOf(file.reader())) = .{ .unbuffered_reader = file.reader() };
+    var de = std.compress.zlib.decompressor(br.reader());
+
+    try de.reader().skipUntilDelimiterOrEof('\x00');
+    var buf: [8192]u8 = undefined;
+    while (true) {
+        const n = try de.reader().readAll(&buf);
+        if (n == 0) break;
+        try stdout.writeAll(buf[0..n]);
     }
 }
