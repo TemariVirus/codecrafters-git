af67707136959fd2c445c8fd7b5d26243a525d58
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2025-04-29 22:11:37 +0800

    Read a tree object #kp1

--- src/main.zig
+++ src/main.zig
@@ -44,11 +44,40 @@ pub fn main() !void {
         }
 
         try hashObject(stdout, file, write);
+    } else if (strEql(command, "ls-tree")) {
+        if (args.len < 3) {
+            try std.io.getStdErr().writer().print("Usage: {s} ls-tree [--name-only] <hash>\n", .{args[0]});
+            return;
+        }
+
+        var name_only = false;
+        var hash: []const u8 = undefined;
+        for (args[2..]) |arg| {
+            if (strEql(arg, "--name-only")) {
+                name_only = true;
+            } else {
+                hash = arg;
+            }
+        }
+
+        try lsTree(stdout, hash, name_only);
     }
 }
 
 fn strEql(a: []const u8, b: []const u8) bool {
     return std.mem.eql(u8, a, b);
+}
+
+fn objectDir(hash: [40]u8) [15]u8 {
+    var buf: [15]u8 = undefined;
+    _ = std.fmt.bufPrint(&buf, ".git/objects/{s}", .{hash[0..2]}) catch unreachable;
+    return buf;
+}
+
+fn objectPath(hash: [40]u8) [54]u8 {
+    var buf: [54]u8 = undefined;
+    _ = std.fmt.bufPrint(&buf, ".git/objects/{s}/{s}", .{ hash[0..2], hash[2..] }) catch unreachable;
+    return buf;
 }
 
 fn init(stdout: AnyWriter) !void {
@@ -65,12 +94,9 @@ fn init(stdout: AnyWriter) !void {
 }
 
 fn catFile(stdout: AnyWriter, hash: []const u8) !void {
-    const allocator = gpa.allocator();
-
     const file = blk: {
-        const path = try std.fmt.allocPrint(allocator, ".git/objects/{s}/{s}", .{ hash[0..2], hash[2..] });
-        defer allocator.free(path);
-        break :blk try fs.cwd().openFile(path, .{});
+        const path = objectPath(hash[0..40].*);
+        break :blk try fs.cwd().openFile(&path, .{});
     };
     defer file.close();
 
@@ -109,10 +135,9 @@ fn hashObject(stdout: AnyWriter, file_path: []const u8, write: bool) !void {
     try stdout.print("{s}\n", .{hash});
 
     if (write) {
-        const path = try std.fmt.allocPrint(allocator, ".git/objects/{s}/{s}", .{ hash[0..2], hash[2..] });
-        defer allocator.free(path);
-        try fs.cwd().makePath(path[0..".git/objects/00".len]);
-        const obj = try fs.cwd().createFile(path, .{});
+        const path = objectPath(hash);
+        try fs.cwd().makePath(&objectDir(hash));
+        const obj = try fs.cwd().createFile(&path, .{});
         defer obj.close();
 
         try file.seekTo(0);
@@ -126,5 +151,51 @@ fn hashObject(stdout: AnyWriter, file_path: []const u8, write: bool) !void {
             _ = try compressor.write(buf[0..n]);
         }
         _ = try compressor.finish();
+    }
+}
+
+fn lsTree(stdout: AnyWriter, hash: []const u8, name_only: bool) !void {
+    const allocator = gpa.allocator();
+
+    const file = blk: {
+        const path = objectPath(hash[0..40].*);
+        break :blk try fs.cwd().openFile(&path, .{});
+    };
+    defer file.close();
+
+    var br: std.io.BufferedReader(8192, @TypeOf(file.reader())) = .{ .unbuffered_reader = file.reader() };
+    var de = std.compress.zlib.decompressor(br.reader());
+
+    {
+        var tree_buf: [4]u8 = undefined;
+        _ = try de.reader().readAll(&tree_buf);
+        if (!strEql(&tree_buf, "tree")) {
+            return error.NotATree;
+        }
+    }
+
+    try de.reader().skipUntilDelimiterOrEof('\x00');
+    while (true) {
+        const mode = try de.reader().readUntilDelimiterOrEofAlloc(allocator, ' ', 256) orelse break;
+        defer allocator.free(mode);
+        const object_type = if (strEql(mode, "40000")) "tree" else "blob";
+
+        // There probably aren't filesystems that support 1MB filenames
+        const name = try de.reader().readUntilDelimiterAlloc(allocator, '\x00', 1024 * 1024);
+        defer allocator.free(name);
+
+        var object_hash: [20]u8 = undefined;
+        if (try de.reader().readAll(&object_hash) != object_hash.len) {
+            return error.CorruptedFile;
+        }
+
+        if (!name_only) {
+            try stdout.print("{s} {s} {}\t", .{
+                mode,
+                object_type,
+                std.fmt.fmtSliceHexLower(&object_hash),
+            });
+        }
+        try stdout.print("{s}\n", .{name});
     }
 }
